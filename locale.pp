c9cd5253f70fdeabb297c7d37d37c9ca9bfe8592
Daniel Sobral <dcsobral@gmail.com>
2011-04-08 20:56:54 -0300

    Refresh

--- locale.pp
+++ locale.pp
@@ -21,7 +21,8 @@ file { '/etc/locale.gen':
 }
 
 exec { '/usr/sbin/locale-gen':
-    subscribe => File['/etc/locale.gen'],
-    require   => [ Package['locales'], File['/etc/locale.gen'] ],
+    subscribe   => File['/etc/locale.gen'],
+    refreshonly => true,
+    require     => [ Package['locales'], File['/etc/locale.gen'] ],
 }
 

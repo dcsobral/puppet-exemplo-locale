8d1c15a1904c3da2edab3549823db1efbbb817b3
Daniel Sobral <dcsobral@gmail.com>
2011-04-08 20:58:06 -0300

    Classes e nós

--- locale.pp
+++ locale.pp
@@ -1,28 +1,34 @@
-package { 'locales':
-	ensure => installed,
+class locale {
+    package { 'locales':
+        ensure => installed,
+    }
+
+    file { '/etc/default/locale':
+        ensure  => present,
+        content => 'LANG=pt_BR.UTF-8',
+        require => Package['locales'],
+        owner   => 'root',
+        group   => 'root',
+        mode    => 644,
+    }
+
+    file { '/etc/locale.gen':
+        ensure  => present,
+        source  => '/root/exemplo/locale.gen',
+        require => Package['locales'],
+        owner   => 'root',
+        group   => 'root',
+        mode    => 644,
+    }
+
+    exec { '/usr/sbin/locale-gen':
+        subscribe   => File['/etc/locale.gen'],
+        refreshonly => true,
+        require     => [ Package['locales'], File['/etc/locale.gen'] ],
+    }
 }
 
-file { '/etc/default/locale':
-	ensure  => present,
-	content => 'LANG=pt_BR.UTF-8',
-	require => Package['locales'],
-	owner   => 'root',
-	group   => 'root',
-	mode    => 644,
-}
-
-file { '/etc/locale.gen':
-	ensure  => present,
-	source  => '/root/exemplo/locale.gen',
-	require => Package['locales'],
-	owner   => 'root',
-	group   => 'root',
-	mode    => 644,
-}
-
-exec { '/usr/sbin/locale-gen':
-    subscribe   => File['/etc/locale.gen'],
-    refreshonly => true,
-    require     => [ Package['locales'], File['/etc/locale.gen'] ],
+node default {
+    include locale
 }
 
